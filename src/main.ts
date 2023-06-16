9bd96cf9672dabd4c54b59002e419a6fc88ee682
DlopezS98 <01dlopezs98@gmail.com>
2023-06-15 19:14:34 -0600

    setting up webhooks

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+// import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true });
   app.enableCors();
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  
+  app.useBodyParser('json', { limit: '50mb' });
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // app.use('/stripe/webhooks', bodyParser.raw({ type: 'application/json' }));
+  // app.use(bodyParser.json());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
 import Stripe from 'stripe';
+import { Request } from "express";
+import { BadRequestException, Body, Controller, Get, Post, RawBodyRequest, Req } from "@nestjs/common";
 import CheckoutSessionDto, { CreateCheckoutSessionDto } from "./dtos/checkout-session.dto";
 
 const STRIPE_SECRET_KEY = '';
 
 @Controller('stripe')
 export default class StripeController {
+  private readonly stripe: Stripe;
+
+  constructor() {
+    this.stripe = new Stripe(STRIPE_SECRET_KEY, { typescript: true, apiVersion: '2022-11-15' });
+  }
+
   @Post('create-checkout-session')
   async createSession(@Body() createSessionDto: CreateCheckoutSessionDto): Promise<CheckoutSessionDto> {
-    const stripe = new Stripe(STRIPE_SECRET_KEY, { typescript: true, apiVersion: '2022-11-15' });
-    const session = await stripe.checkout.sessions.create({
+    const session = await this.stripe.checkout.sessions.create({
       mode: 'subscription',
       payment_method_types: ['card'],
       line_items: [{ price: createSessionDto.priceId, quantity: 1 }],
@@ -22,8 +28,35 @@ export default class StripeController {
 
   @Get('products')
   async getProducts(): Promise<Stripe.Product[]> {
-    const stripe = new Stripe(STRIPE_SECRET_KEY, { typescript: true, apiVersion: '2022-11-15' });
-    const products = await stripe.products.list({ active: true, expand: ['data.default_price'] });
+    Stripe.SubscriptionsResource
+    const products = await this.stripe.products.list({ active: true, expand: ['data.default_price'] });
     return products.data;
+  }
+
+  @Post('webhook')
+  async webhook(@Req() request: RawBodyRequest<Request>): Promise<string> {
+    const signature = request.header('stripe-signature');
+    if (!signature) throw new BadRequestException('Missing stripe-signature header');
+
+    const endpointScrete = '';
+    const rawBody = request.rawBody as Buffer;
+    const event = this.stripe.webhooks.constructEvent(rawBody, signature, endpointScrete);
+    switch (event.type) {
+      case 'customer.subscription.created':
+      case 'customer.subscription.deleted':
+        console.log(event);
+        break;
+      case 'invoice.payment_succeeded':
+        console.log(event);
+        break;
+      case 'invoice.payment_failed':
+        console.log(event);
+        break;
+      case 'checkout.session.completed':
+        console.log(event);
+      default: break;
+    }
+
+    return 'success';
   }
 }
